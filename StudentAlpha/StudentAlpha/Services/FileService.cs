6becd23880a8a3fc9b2b7797172cdb1f81e9a347
ChungWei <ChungWei@LCW-PC>
2016-11-01 20:44:02 +0800

    Added FileService

---
+++ StudentAlpha/StudentAlpha/Services/FileService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace StudentAlpha.Services
+{
+    public class FileService
+    {
+        public async Task WriteDataToLocalStorageAsync(string fileName, string content)
+        {
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            StorageFile file = await folder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            await FileIO.WriteTextAsync(file, content);
+        }
+
+        public async Task<string> ReadDataFromLocalStorageAsync(string fileName)
+        {
+            string result = null;
+            StorageFolder folder = ApplicationData.Current.LocalFolder;
+            StorageFile file = await folder.GetFileAsync(fileName);
+            result = await FileIO.ReadTextAsync(file);
+            return result;
+        }
+    }
+}
