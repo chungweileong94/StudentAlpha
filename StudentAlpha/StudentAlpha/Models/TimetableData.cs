fea0dcfdc308cd241f64e10e6e07ddb09a07345f
ChungWei <ChungWei@LCW-PC>
2016-11-04 01:41:51 +0800

    Added Timetable Design

---
+++ StudentAlpha/StudentAlpha/Models/TimetableData.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace StudentAlpha.Models
+{
+    public class TimetableData : INotifyPropertyChanged
+    {
+        private string _Subject;
+        public string Subject
+        {
+            get { return _Subject; }
+            set { Set(ref _Subject, value); }
+        }
+
+        private string _Lecture;
+        public string Lecture
+        {
+            get { return _Lecture; }
+            set { Set(ref _Lecture, value); }
+        }
+
+        private string _Venue;
+
+        public string Venue
+        {
+            get { return _Venue; }
+            set { Set(ref _Venue, value); }
+        }
+
+        private DateTime _StartDateTime;
+        public DateTime StartDateTime
+        {
+            get { return _StartDateTime; }
+            set
+            {
+                Set(ref _StartDateTime, value);
+                StartTimeStringFormat = _StartDateTime.ToString("h:mm tt");
+            }
+        }
+
+        private DateTime _EndDateTime;
+        public DateTime EndDateTime
+        {
+            get { return _EndDateTime; }
+            set
+            {
+                Set(ref _EndDateTime, value);
+                EndTimeStringFormat = _EndDateTime.ToString("h:mm tt");
+            }
+        }
+
+        private string _StartTimeStringFormat;
+        public string StartTimeStringFormat
+        {
+            get { return _StartTimeStringFormat; }
+            set { Set(ref _StartTimeStringFormat, value); }
+        }
+
+        private string _EndTimeStringFormat;
+        public string EndTimeStringFormat
+        {
+            get { return _EndTimeStringFormat; }
+            set { Set(ref _EndTimeStringFormat, value); }
+        }
+
+        #region INotifyPropertyChanged Helper
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(storage, value))
+                return false;
+            storage = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+        #endregion
+    }
+
+}
--- StudentAlpha/StudentAlpha/Views/SubViews/TimetablePage.xaml.cs
+++ StudentAlpha/StudentAlpha/Views/SubViews/TimetablePage.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using StudentAlpha.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -9,22 +11,103 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Hosting;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace StudentAlpha.Views.SubViews
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class TimetablePage : Page
     {
+        public List<TimetableData> list { get; set; }
+
         public TimetablePage()
         {
             this.InitializeComponent();
+
+            list = new List<TimetableData>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new TimetableData()
+                {
+                    Subject = $"Subject {i}",
+                    StartDateTime = DateTime.Now,
+                    EndDateTime = DateTime.Now.AddHours(1),
+                    Lecture = $"Lecture {i}"
+                });
+            }
+        }
+
+        private void Pivot_Loaded(object sender, RoutedEventArgs e)
+        {
+            var s = sender as Pivot;
+            s.SelectedIndex = (int)DateTime.Now.DayOfWeek;
+        }
+
+        private void GridView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var s = sender as GridView;
+
+            if (e.NewSize.Width < 500)
+            {
+                ((ItemsWrapGrid)s.ItemsPanelRoot).ItemWidth = e.NewSize.Width;
+            }
+            else
+            {
+                ((ItemsWrapGrid)s.ItemsPanelRoot).ItemWidth = double.NaN;
+            }
+        }
+
+        private GridView currentGridView;
+
+        private void GridView_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (!args.InRecycleQueue)
+            {
+                currentGridView = sender as GridView;
+                args.ItemContainer.Loaded += ItemContainer_Loaded;
+            }
+        }
+
+        private void ItemContainer_Loaded(object sender, RoutedEventArgs e)
+        {
+            var itemContainer = sender as GridViewItem;
+            var itempanel = currentGridView.ItemsPanelRoot as ItemsWrapGrid;
+            var itemIndex = currentGridView.IndexFromContainer(itemContainer);
+
+            var sp = itemContainer.ContentTemplateRoot as StackPanel;
+
+            if (itemIndex >= itempanel.FirstVisibleIndex && itemIndex <= itempanel.LastVisibleIndex)
+            {
+                var width = (float)sp.RenderSize.Width;
+                var height = (float)sp.RenderSize.Height;
+
+                var _compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
+                var visual = ElementCompositionPreview.GetElementVisual(itemContainer);
+                visual.Size = new Vector2(width, height);
+                visual.Scale = new Vector3(1, 1, 1);
+                visual.Offset = new Vector3(0, 0, 0);
+                visual.CenterPoint = new Vector3(width / 2, height / 2, 0);
+                visual.Opacity = 0f;
+
+                var scaleAnimation = _compositor.CreateVector3KeyFrameAnimation();
+                scaleAnimation.InsertKeyFrame(0f, new Vector3(0, 0, 0));
+                scaleAnimation.InsertKeyFrame(0.1f, new Vector3(.5f, .5f, .5f));
+                scaleAnimation.InsertKeyFrame(1f, new Vector3(1f, 1f, 1f));
+                scaleAnimation.Duration = TimeSpan.FromMilliseconds(400);
+                scaleAnimation.DelayTime = TimeSpan.FromMilliseconds(itemIndex * 150);
+
+                var fadeAnimation = _compositor.CreateScalarKeyFrameAnimation();
+                fadeAnimation.InsertExpressionKeyFrame(1f, "1");
+                fadeAnimation.Duration = TimeSpan.FromMilliseconds(1500);
+                fadeAnimation.DelayTime = TimeSpan.FromMilliseconds(itemIndex * 150);
+
+                visual.StartAnimation("Scale", scaleAnimation);
+                visual.StartAnimation("Opacity", fadeAnimation);
+            }
+            itemContainer.Loaded -= ItemContainer_Loaded;
         }
     }
 }
